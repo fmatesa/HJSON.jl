960ba52dacbb7bae0c482ef1a6a2984cd23775b3
Fabian Mateša <fabian.matesa@zellerfeld.com>
2024-09-25 16:08:37 +0200

    Added test cases

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,31 @@
 using HJSON
+using JSON
 using Test
 
 @testset "HJSON.jl" begin
-    # Write your tests here.
+    @testset "read_hjson" begin
+        dict = HJSON.read_hjson("test.hjson")
+
+        @test dict["test"] === "test"
+    end
+
+    @testset "to_json" begin
+        HJSON.to_json("test.hjson", "result.json")
+        @test isfile("result.json")
+
+        dict = JSON.parsefile("result.json", use_mmap=false)
+        @test dict["test"] === "test"
+
+        rm("result.json")
+    end
+
+    @testset "to_hjson" begin
+        HJSON.to_json("test.json", "result.hjson")
+        @test isfile("result.hjson")
+
+        dict = HJSON.read_hjson("result.hjson")
+        @test dict["test"] === "test"
+
+        rm("result.hjson")
+    end
 end
